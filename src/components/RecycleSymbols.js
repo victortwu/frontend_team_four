2c081ffbaa51404eb95dec40659dbe59295cbbca
victortwu <victor607@gmail.com>
2021-10-10 21:11:16 -0700

    refactored some css to be component specific

--- src/components/RecycleSymbols.js
+++ src/components/RecycleSymbols.js
@@ -2,16 +2,44 @@ import React from 'react'
 
 const RecycleSymbols = (props) => {
 console.log(props.recTypeData)
+
+// wasn't sure how to do this with tailwind so just going to inject the css inline.  Also, an effort to keep this component more encapsulated
+
+// in fact, api call can happen here instead of passing in props 
+const gridPosition = {
+  position: 'absolute',
+  left: '0',
+  top: '0',
+  right: '0',
+  bottom: '0',
+  padding: '1rem',
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gridTemplateRows: '1fr 1fr 1fr 1fr',
+  gridTemplateAreas: `
+                      'area1 area2'
+                      'area3 area4'
+                      'area5 area6'
+                      'area7 areaQ'
+                      `,
+  gridGap: '1rem'
+}
+
   return(
     <div className='container absolute top-10 right-20 w-80 h-5/6 bg-blue-100 rounded-md shadow-lg z-10'>
-      <div id='recIconCtn'>
+      <div style={gridPosition}>
 
         {
           props.recTypeData.map(thing => {
             const gridSpot = `area${thing.recycleNumber}`
-            return <div key={thing._id} id={gridSpot} className='w-full h-full bg-gray-100 text-blue-300 text-xl rounded-md shadow-sm flex justify-center items-center'>
-              {thing.recycleNumber}
-            </div>
+            return <div
+                      key={thing._id}
+                      id={gridSpot}
+                      style={{gridArea: gridSpot}}
+                      className='w-full h-full bg-gray-100 text-blue-300 text-xl rounded-md shadow-sm flex justify-center items-center'
+                      >
+                      {thing.recycleNumber}
+                    </div>
           })
         }
 
