4f762fa707d798331b245da2990c70c23a1ed84f
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-10-18 21:16:59 -0500

    basic form implemented to receive upc

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Search = ()=> {
+
+    const [upc, setUpc] = useState()
+
+    const handleChange = (e) =>{
+        e.preventDefault();
+        setUpc(e.target.value)
+    }
+
     return (
         <div>
             <h1>Search Page</h1>
+            <form>
+                <input
+                onChange={handleChange}
+                type="text"
+                placeholder="Enter UPC"
+                />
+            </form>
         </div>
     )
 }
