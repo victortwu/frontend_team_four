0d4e0124fb07255addfee0c9732f185f18e5cba9
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-10-18 21:33:29 -0500

    aonSubmit with push to display page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
+import { useHistory } from 'react-router'
 
 const Search = ()=> {
+
+    let history = useHistory()
 
     const [upc, setUpc] = useState()
 
@@ -9,15 +12,21 @@ const Search = ()=> {
         setUpc(e.target.value)
     }
 
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        history.push(`/display/${upc}`)
+    }
+
     return (
         <div>
             <h1>Search Page</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                 onChange={handleChange}
                 type="text"
                 placeholder="Enter UPC"
                 />
+                <button type="submit">Search</button>
             </form>
         </div>
     )
