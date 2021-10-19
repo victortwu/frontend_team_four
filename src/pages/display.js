7594807bb8cfbe7daa8b03659d1f2aee30d3d545
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-10-19 00:09:14 -0500

    async api call with redirect to homepage if UPC not found in database

--- src/pages/display.js
+++ src/pages/display.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useEffect } from 'react'
+import { useParams, useHistory } from 'react-router'
 
 
 const Display = ()=>{
 
-    const urlParam = useParams()
+    const urlParam = useParams();
     const upc = urlParam['upc']
-    console.log(upc)
+    const history = useHistory()
+
+    const getUpcInfo =async()=>{
+        const response = await fetch (`http://localhost:5000/upc/${upc}`)
+        const responseData = await response.json()
+        console.log(responseData)
+        if (responseData['success']==false){
+            history.push('/')
+        }
+    }
+
+
+    useEffect(()=>{getUpcInfo()},[])
+    
+    
 
     return (
         <div>
